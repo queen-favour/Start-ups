bfc0e0a344708d58441e47e7f9300724ac0b961f
queen-favour <elechifavour3@gmail.com>
2024-11-23 07:07:45 +0100

    chore

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import NavBar from "../../components/NavBar/NavBar";
 export default function Home() {
   return (
     <>
-      <div className="hidden md:flex gap-8 text-xl">
+      <div className=" md:flex gap-8 text-xl">
         <a href="/login">Login</a>
         <a href="/Startups">after login</a>{" "}
       </div>
